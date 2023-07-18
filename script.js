f7d1bee8a43e9be2fa1cd43136bcaee321e5562c
ClaudiaV27 <claudiavielmetti38@gmail.com>
2023-07-18 13:15:31 +0200

    computer paddles

--- script.js
+++ script.js
@@ -13,7 +13,8 @@ let lastTime;
 function update(time) {
   if (lastTime != null) {
     const delta = time - lastTime;
-    // ball.update(delta);
+    // ball.update(delta)
+    computerPaddle.update(delta, ball.y)
   }
   lastTime = time;
   window.requestAnimationFrame(update);
