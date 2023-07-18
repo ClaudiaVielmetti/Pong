e2554aa8136d650ed363ab69c11baa894d4048f2
ClaudiaV27 <claudiavielmetti38@gmail.com>
2023-07-18 13:17:48 +0200

    reset elements

--- Paddle.js
+++ Paddle.js
@@ -3,6 +3,7 @@ const SPEED = 0.02;
 export default class Paddle {
   constructor(paddleElem) {
     this.paddleElem = paddleElem;
+    this.reset();
   }
 
   get position() {
@@ -15,7 +16,10 @@ export default class Paddle {
     this.paddleElem.style.setProperty("--position", value);
   }
 
+reset() {
+   this.position = 50; 
+}
   update(delta, ballHeight) {
-    this.position += SPEED * delta * (ballHeight - this.position)
+    this.position += SPEED * delta * (ballHeight - this.position);
   }
 }
