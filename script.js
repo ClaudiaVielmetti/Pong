7cd85ea593fbfd0d516b782e1100fd38c3ed3209
ClaudiaV27 <claudiavielmetti38@gmail.com>
2023-07-18 13:27:34 +0200

    score counter

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // the game will run in an update loop - every frame will have a function called
 // this will update the positions and the logic
+import { handle } from "express/lib/application.js";
 import Ball from "./Ball.js";
 import Paddle from "./Paddle.js";
 
@@ -7,6 +8,8 @@ import Paddle from "./Paddle.js";
 const ball = new Ball(document.getElementById("ball"))
 const playerPaddle = new Paddle(document.getElementById("player-paddle"))
 const computerPaddle = new Paddle(document.getElementById("computer-paddle"))
+const playerScoreElem = document.getElementById("player-score") 
+const computerScoreElem = document.getElementById("computer-score")
 
 //use the ball element inside the update loop
 let lastTime;
@@ -15,9 +18,33 @@ function update(time) {
     const delta = time - lastTime;
     // ball.update(delta)
     computerPaddle.update(delta, ball.y)
+
+    if(isLose()) handleLose() {
+
+    }
   }
+
+
   lastTime = time;
   window.requestAnimationFrame(update);
+}
+
+function isLose(){
+  const rect = ball.rect()
+  return rect.right >= window.innerWidth || rect.left <= 0
+}
+
+
+function handleLose(){
+  const rect = ball.rect()
+  if(rect.right >= window.innerWidth ){
+    playerScoreElem.textContent = parseInt(playerScoreElem.textContent) + 1
+  }else {
+    computerScoreElem.textContent = parseInt(computerScoreElem.textContent) + 1
+  }
+    
+  ball.reset()
+  computerPaddle.reset()
 }
 
 document.addEventListener("mousemove", e => {
